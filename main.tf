67dc8fea2c851ff73d36fe83b3b2b72690567c2e
aly432 <aly432@gmail.com>
2024-12-28 15:42:53 -0800

    Update main.tf
    ae-update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "networking" {
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
   cidr_public_subnet   = var.cidr_public_subnet
-  eu_availability_zone = var.eu_availability_zone
+  us_availability_zone = var.us_availability_zone
   cidr_private_subnet  = var.cidr_private_subnet
 }
 
@@ -63,6 +63,6 @@ module "hosted_zone" {
 
 module "aws_ceritification_manager" {
   source         = "./certificate-manager"
-  domain_name    = "jenkins.jhooq.org"
+  domain_name    = "jenkins.agsecure.cloud"
   hosted_zone_id = module.hosted_zone.hosted_zone_id
 }
