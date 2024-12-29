59a8a686bd88b47f7005d948359f1f677af40264
aly432 <aly432@gmail.com>
2024-12-28 17:15:47 -0800

    Update remote_backend_s3.tf

--- remote_backend_s3.tf
+++ remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-jenkins-remote-state-bucket-222456"
     key    = "devops-project-1/jenkins/terraform.tfstate"
-    region = "us-west-1"
+    region = "us-east-1"
   }
 }
